ac6c3fb580a43f23007c052ac347fdb4551d4fbe
najwaadwf <97710562+najwaadwf@users.noreply.github.com>
2024-06-18 07:34:03 +0800

    Update Login_failed.py

--- Login_failed.py
+++ Login_failed.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
-from selenium.webdriver.firefox.service import Service
-from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -8,11 +8,11 @@ import time
 
 website_url = "https://simari.ulm.ac.id/login/module.php/core/loginuserpass.php?AuthState=_9b580da462f35d11c20239dd5f249caa76adddefe9%3Ahttps%3A%2F%2Fsimari.ulm.ac.id%2Flogin%2Fsaml2%2Fidp%2FSSOService.php%3Fspentityid%3Dhttps%253A%252F%252Fsimari.ulm.ac.id%252Fsaml%252Fmetadata%26cookieTime%3D1718339434%26RelayState%3Dhttps%253A%252F%252Fsimari.ulm.ac.id%252Fsaml"
 
-options = webdriver.FirefoxOptions()
+options = webdriver.ChromeOptions()
 options.add_argument("--incognito")
 options.add_argument("--start-maximized")
 
-driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()), options=options)
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 driver.get(website_url)
 time.sleep(5)
 
